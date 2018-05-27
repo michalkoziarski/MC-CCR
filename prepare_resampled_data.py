ca0d7bd602c2c67861599e240df51a74394e65da
michalkoziarski <michalkoziarski@gmail.com>
2018-05-27 13:35:15 +0200

    processing all datasets and folds within a single script execution

--- prepare_resampled_data.py
+++ prepare_resampled_data.py
@@ -1,6 +1,7 @@
 import argparse
 import algorithms
 import datasets
+import logging
 import numpy as np
 import pandas as pd
 
@@ -10,11 +11,10 @@ from pathlib import Path
 DEFAULT_ROOT_OUTPUT_PATH = Path(__file__).parent / 'results'
 DEFAULT_DATA_PATH = Path(__file__).parent / 'data'
 
+logging.basicConfig(level=logging.INFO)
+
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-dataset', choices=datasets.names(), required=True)
-parser.add_argument('-partition', choices=[1, 2, 3, 4, 5], type=int, required=True)
-parser.add_argument('-fold', choices=[1, 2], type=int, required=True)
 parser.add_argument('-mode', choices=['OVA', 'OVO'], default='OVA')
 parser.add_argument('-root_output_path', type=str, default=DEFAULT_ROOT_OUTPUT_PATH)
 parser.add_argument('-energy', type=float, default=0.25)
@@ -24,42 +24,49 @@ parser.add_argument('-method', choices=['sampling', 'complete'], default='sampli
 
 args = parser.parse_args()
 
-output_path = Path(args.root_output_path) / args.dataset
-output_path.mkdir(parents=True, exist_ok=True)
+for dataset in datasets.names():
+    logging.info('Processing dataset "%s"...' % dataset)
 
-(X_train, y_train), (X_test, y_test) = datasets.load(args.dataset, args.partition, args.fold)
+    for partition in [1, 2, 3, 4, 5]:
+        for fold in [1, 2]:
+            logging.info('Processing fold %dx%d...' % (partition, fold))
 
-header = pd.read_csv(
-    DEFAULT_DATA_PATH / 'folds' / args.dataset / ('%s.%d.%d.train.csv' % (args.dataset, args.partition, args.fold))
-).columns
+            output_path = Path(args.root_output_path) / dataset
+            output_path.mkdir(parents=True, exist_ok=True)
 
-if args.mode == 'OVA':
-    X_train, y_train = algorithms.MultiClassCCR(
-        energy=args.energy, cleaning_strategy=args.cleaning_strategy, p_norm=args.p_norm, method=args.method
-    ).fit_sample(X_train, y_train)
+            (X_train, y_train), (X_test, y_test) = datasets.load(dataset, partition, fold)
 
-    csv_path = output_path / ('%s.%d.%d.train.oversampled.csv' % (args.dataset, args.partition, args.fold))
+            header = pd.read_csv(
+                DEFAULT_DATA_PATH / 'folds' / dataset / ('%s.%d.%d.train.csv' % (dataset, partition, fold))
+            ).columns
 
-    pd.DataFrame(np.c_[X_train, y_train]).to_csv(csv_path, index=False, header=header)
-elif args.mode == 'OVO':
-    classes = np.unique(np.concatenate([y_train, y_test]))
+            if args.mode == 'OVA':
+                X_train, y_train = algorithms.MultiClassCCR(
+                    energy=args.energy, cleaning_strategy=args.cleaning_strategy, p_norm=args.p_norm, method=args.method
+                ).fit_sample(X_train, y_train)
 
-    for i in range(len(classes)):
-        for j in range(i + 1, len(classes)):
-            indices = ((y_train == classes[i]) | (y_train == classes[j]))
+                csv_path = output_path / ('%s.%d.%d.train.oversampled.csv' % (dataset, partition, fold))
 
-            X, y = X_train[indices].copy(), y_train[indices].copy()
+                pd.DataFrame(np.c_[X_train, y_train]).to_csv(csv_path, index=False, header=header)
+            elif args.mode == 'OVO':
+                classes = np.unique(np.concatenate([y_train, y_test]))
 
-            class_distribution = {cls: len(y[y == cls]) for cls in [classes[i], classes[j]]}
-            minority_class = min(class_distribution, key=class_distribution.get)
+                for i in range(len(classes)):
+                    for j in range(i + 1, len(classes)):
+                        indices = ((y_train == classes[i]) | (y_train == classes[j]))
 
-            X, y = algorithms.CCR(
-                energy=args.energy, cleaning_strategy=args.cleaning_strategy, p_norm=args.p_norm
-            ).fit_sample(X, y)
+                        X, y = X_train[indices].copy(), y_train[indices].copy()
 
-            csv_path = output_path / ('%s.%d.%d.train.oversampled.%dv%d.csv' %
-                                      (args.dataset, args.partition, args.fold, classes[i], classes[j]))
+                        class_distribution = {cls: len(y[y == cls]) for cls in [classes[i], classes[j]]}
+                        minority_class = min(class_distribution, key=class_distribution.get)
 
-            pd.DataFrame(np.c_[X, y]).to_csv(csv_path, index=False, header=header)
-else:
-    raise NotImplementedError
+                        X, y = algorithms.CCR(
+                            energy=args.energy, cleaning_strategy=args.cleaning_strategy, p_norm=args.p_norm
+                        ).fit_sample(X, y)
+
+                        csv_path = output_path / ('%s.%d.%d.train.oversampled.%dv%d.csv' %
+                                                  (dataset, partition, fold, classes[i], classes[j]))
+
+                        pd.DataFrame(np.c_[X, y]).to_csv(csv_path, index=False, header=header)
+            else:
+                raise NotImplementedError
