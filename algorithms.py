1e2005d981b25c77339b14967a6e8015210664f8
michalkoziarski <michalkoziarski@gmail.com>
2018-05-27 14:17:41 +0200

    back to sampling with replacement: necessary in same cases in which n < population size

--- algorithms.py
+++ algorithms.py
@@ -156,7 +156,7 @@ class MultiClassCCR:
 
                 for j in range(0, i):
                     all_indices = list(range(len(observations[classes[j]])))
-                    used_indices = np.random.choice(all_indices, int(n_max / i), replace=False)
+                    used_indices = np.random.choice(all_indices, int(n_max / i))
 
                     used_observations[classes[j]] = [
                         observations[classes[j]][idx] for idx in all_indices if idx in used_indices
